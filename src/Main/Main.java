1684878fa33e8e5c6efc48dfdaec08947cc0fc2a
Annely <annely.vattis@gmail.com>
2016-10-13 16:32:52 +0300

    Algus

---
+++ src/Main/Main.java
@@ -0,0 +1,7 @@
+// Faili ja Classi nimi peavad olema samad
+public class Main {
+    public static void main(String args[]) {
+        System.out.println(“Alustame pommitamisega”);
+        
+    }
+}
